72666e257d521d4820cf629637408ddf9317b054
Martin Ethier <methier@edu.uwaterloo.ca>
2020-04-18 16:17:47 -0400

    boi

--- models/feature_extractor.py
+++ models/feature_extractor.py
@@ -5,6 +5,9 @@ import torchvision
 class FeatureExtractor(torch.nn.Module):
     """
     Initializes a pretrained ImageNet feature extractor from torchvision.models
+    Input images have to be at least 224x224, scaled to be within 0 and 1, then normalized using:
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
     """
 
     def __init__(self, model_name, input_size):
