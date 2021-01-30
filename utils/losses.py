67bc2b786c406cd8e4fa53e5525da6ef10f64adf
Martin Ethier <methier@edu.uwaterloo.ca>
2021-01-30 01:02:30 -0500

    Added grad loss

--- utils/losses.py
+++ utils/losses.py
@@ -11,3 +11,24 @@ def finite_diff(batched_path):
     
     return grads
 
+def grad_l1_loss(label, output):
+    label_grads = finite_diff(label)
+    output_reshaped = output.view(label.shape)
+    output_grads = finite_diff(output_reshaped)
+    loss = torch.nn.L1Loss()(label_grads, output_grads)
+    
+    return loss
+
+def time_weighted_l1_loss(label, output):
+    output_reshaped = output.view(label.shape)
+    
+    loss = torch.nn.L1Loss(reduction='none')(label, output_reshaped)
+    
+    t = torch.arange(label.shape[1])
+    weights = (-torch.exp(-t/10) + 1.01).to(loss.device)
+    
+    tiled_weights = weights.unsqueeze(1).repeat(label.shape[0], 1, 3)
+    
+    weighted_loss = torch.mean(tiled_weights * loss)
+    
+    return weighted_loss
